f3cf96d582cdef8a6819a612c25f991c0542a50c
AladdinYilmaz <aladdinyilmaz@gmail.com>
2022-08-09 10:54:02 +0300

    first

---
+++ src/test/java/Test/_01LoginTest.java
@@ -0,0 +1,54 @@
+package Test;
+
+import POM.HomePageElements;
+import POM.LoginPageElements;
+import POM.MyAccountPageElements;
+import Utils.BaseDriver;
+
+public class _01LoginTest extends BaseDriver {
+    HomePageElements homePageElements;
+    LoginPageElements loginPageElements;
+    MyAccountPageElements myAccountPageElements;
+
+    @Test
+    public void loginPositiveTest() {
+
+        homePageElements = new HomePageElements(driver);
+        loginPageElements = new LoginPageElements(driver);
+        myAccountPageElements = new MyAccountPageElements(driver);
+//
+//        homePageElements.myAccountButton.click();
+//        homePageElements.loginButton.click();
+//
+//        loginPageElements.emailInput.sendKeys("test@technostudy.com");
+//        loginPageElements.passwordInput.sendKeys("Test12345..");
+//        loginPageElements.loginButton.click();
+//
+//        String myAccountHeaderText = myAccountPageElements.myAccountHeader.getText();
+//
+//        Assert.assertEquals(myAccountHeaderText, "My Account");
+
+    }
+
+//    @Test
+//    public void loginNegativeTest() {
+//
+//        homePageElements = new HomePageElements(driver);
+//        loginPageElements = new LoginPageElements(driver);
+//
+//        homePageElements.myAccountButton.click();
+//        homePageElements.loginButton.click();
+//
+//        loginPageElements.emailInput.sendKeys("test@technostudy.com");
+//        loginPageElements.passwordInput.sendKeys("Test1234!!!");
+//        loginPageElements.loginButton.click();
+//
+//        String errorActualText = loginPageElements.errorMessage.getText();
+//        boolean isErrorDisplayed = loginPageElements.errorMessage.isDisplayed();
+//
+//        Assert.assertTrue(isErrorDisplayed);
+//        Assert.assertEquals(errorActualText, "Warning: No match for E-Mail Address and/or Password.");
+//
+//    }
+
+}
---
+++ src/test/java/Utils/BaseDriver.java
@@ -0,0 +1,38 @@
+package Utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseDriver {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+
+       // System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("http://opencart.abstracta.us/");
+
+    }
+
+   // @AfterMethod
+    public void tearDown() {
+
+        driver.quit();
+
+    }
+
+
+
+}
